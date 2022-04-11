50b09921ae36a11f7d5bd8972e6d2dffc581a3df
Divyansh Katare <93079133+KDivyansh@users.noreply.github.com>
2022-04-11 21:00:29 +0530

    Update area.py

--- area.py
+++ area.py
@@ -1,3 +1,3 @@
-radius  = float(input("Input the radius of circle : "))
-area = (3.14159265359) * radius * radius
-print("The area of the circle with radius ",radius," is:", area)
+radius  = float(input("Input the radius of circle : ")) #Taking Input of Radius
+area = (3.14159265359) * radius * radius #Calculating Area
+print("The area of the circle with radius ",radius," is:", area) #Printing Area
