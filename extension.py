c74a25a6027951ce4d0a7162c14ce932ac52d69d
Divyansh Katare <93079133+KDivyansh@users.noreply.github.com>
2022-04-11 21:06:55 +0530

    Assignment-2
    Your second task now is to write a Python program to accept a filename from the user and print the extension of that.
    Your Input and Output should look something like this
    
    Input the Filename: abc.py The extension of the file is : 'python'

---
+++ extension.py
@@ -0,0 +1,6 @@
+filename= input("Enter Filename: ")
+#accepting filename
+ext= filename.split(".")
+#seperating extention
+print ("Extension of the filename",filename," is : " + ext[-1])
+#printing extension 
